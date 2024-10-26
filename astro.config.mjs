95010becfe8a4dfdf4e1b2b2d52adff499be6199
alejooroncoy <adoa2705@gmail.com>
2024-10-26 10:16:40 -0500

    feat: Add react to Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 
+import react from '@astrojs/react';
+
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react()]
 });
